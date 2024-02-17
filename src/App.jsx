4e76ecc644b507f88f452ff0372adc2ae6d24fc5
Nabila Nuzhat Moin <e.nabilanuzhat@gmail.com>
2024-02-17 21:04:08 +0600

    Header added in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
 
 const App = () => {
     return (
         <div>
-                <h1>Starting point</h1>
+                <Header></Header>
                 <Outlet></Outlet>
+                {/* Footer */}
         </div>
     );
 };
