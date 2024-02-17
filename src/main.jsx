9a9924053545195d565f74f6f38d6bb93bad9447
Nabila Nuzhat Moin <e.nabilanuzhat@gmail.com>
2024-02-17 20:42:51 +0600

    Reacct routes added and components added

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,38 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter } from 'react-router-dom'
+import { RouterProvider } from 'react-router'
+import Home from './components/Home.jsx'
+import About from './components/About.jsx'
+
+
+
+const router = createBrowserRouter([{
+  path: '/',
+  element: <App />,
+  children: [{
+    path: '/',
+    element: <Home></Home>,
+  },
+
+  {
+    path:'about',
+    element: <About></About>,
+  }
+
+
+],
+},
+
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    
+    <RouterProvider router={router}>
+
+    </RouterProvider>
+    
   </React.StrictMode>,
 )
