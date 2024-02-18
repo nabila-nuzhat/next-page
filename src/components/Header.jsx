d93123cb5d7534a4644098d2c9d427c5a62dcacb
Nabila Nuzhat Moin <e.nabilanuzhat@gmail.com>
2024-02-18 19:56:19 +0600

    Navlinks added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BoltIcon } from '@heroicons/react/24/solid'
 import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
+
+// state for Menu Button
+const [isMenuOpen, setIsMenuOpen] = useState(false)
+
     return (
-        <div className='bg-gray-100 flex justify-between items-center'>
+        <div className='bg-gray-100 px-4 py-5 mx-auto sm:max-w-xl lg:max-w-screen-xl md:px-24 lg:px-8'>
+        
+            <div className=' flex justify-between items-center'>
             {/* Logo section */}
             {/* <div className=' inline-flex items-center'>
                 <BoltIcon className=' h-6 text-blue-500'></BoltIcon>
@@ -17,13 +23,37 @@ const Header = () => {
 
 
             {/* Nav item section */}
-            <ul>
+            <ul className='items-center hidden lg:flex  space-x-8'>
                 <li>
-                    <NavLink to='/'>
-
+                    <NavLink to='/'
+                    title='Home link'
+                    className={({isActive})=> isActive ? 'text-blue-600' : 'default'}>
+                        Home
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to='/books'
+                    title='Books Link'
+                    className={({isActive})=> isActive ? 'text-blue-600' : 'default'}>
+                        Books
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to='/about'
+                    title='About Link'
+                    className={({isActive})=> isActive ? 'text-blue-600' : 'default'}>
+                        About Us
                     </NavLink>
                 </li>
             </ul>
+
+{/* Mobile Navbar Section */}
+            
+
+        </div>
+
+       
+
         </div>
     );
 };
