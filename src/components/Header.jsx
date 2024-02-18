fa2828ca0cc763387936a5eeccc7ca94f5c8a7a9
Nabila Nuzhat Moin <e.nabilanuzhat@gmail.com>
2024-02-18 12:27:20 +0600

    navbar Logo, icon add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import { BoltIcon } from '@heroicons/react/24/solid'
+import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            <h2>Header</h2>
+        <div className='bg-gray-100 flex justify-between items-center'>
+            {/* Logo section */}
+            {/* <div className=' inline-flex items-center'>
+                <BoltIcon className=' h-6 text-blue-500'></BoltIcon>
+                <span className=' ml-2 text-xl font-bold tracking-wide text-gray-800'>nextPage</span>
+            </div> */}
+            <Link to='/' className=' inline-flex items-center'>
+                <BoltIcon className=' h-6 text-blue-500'></BoltIcon>
+                <span className=' ml-2 text-xl font-bold tracking-wide text-gray-800'>nextPage</span>
+            </Link>
+
+
+            {/* Nav item section */}
+            <ul>
+                <li>
+                    <NavLink to='/'>
+
+                    </NavLink>
+                </li>
+            </ul>
         </div>
     );
 };
