17a258a188c88a43b8e984dc6e4b9e83398c6d9a
Nabila Nuzhat Moin <e.nabilanuzhat@gmail.com>
2024-02-20 12:23:36 +0600

    fetch data using Loader & useLoaderData

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Books = () => {
+/** Previous method of fetch Data: using JS
+ *  const [books, setBooks] = useState([])
+ * 
+ *  useEffect(() => {
+ *      fetch('https://api.itbook.store/1.0/new') 
+ *      .then(res => res.json())
+ *      .then(data => console.log(data))
+ *   }, [])
+ * 
+ * NEW method: Using Loader Function, nije nije Fetch kore ready kkore rakhe, tai loading quick hoy
+ */
+
+
+// NEW method fetch data: Using Loader Function
+const {books} = useLoaderData()
+console.log(books);
     return (
-        <div>
-            Books Coming soon...
+        <div className='my-container '>
+            <div className="grid gap-6 mb-8 lg:grid-cols-4 sm:grid-cols-2">
+                {books.map(book =>
+                    <div className=' overflow-hidden'>
+                        <img src= {book.image} alt="book-cover" className='object-cover w-full h-56 md:h-64 xl:h-80' />
+                    </div>
+                    
+                )}
+            </div>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -26,6 +26,7 @@ const router = createBrowserRouter([{
   {
     path:'books',
     element: <Books></Books>,
+    loader: () => fetch('https://api.itbook.store/1.0/new'),
   },
 
 
