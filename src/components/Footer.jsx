5fb16639f465e050909b1ffab9d23e7de9be73c4
Nabila Nuzhat Moin <e.nabilanuzhat@gmail.com>
2024-02-25 23:18:01 +0600

    footer added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className=' px-4 divide-y relative bottom-0 left-0'>
+           <p className=' text-gray-400 text-center py-6 text-sm'>
+           © 2024 nextPage Inc. || Nabila Nuzhat Moin
+           </p>
+        </footer>
+    );
+};
+
+export default Footer;
